9f7ee68fa9743f1a2136640091b03a2487fde8c2
yury10 <geniocrea@gmail.com>
2020-08-26 09:56:44 -0500

    cambios firebase

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 // Este es el punto de entrada de tu aplicacion
 
-import router from "./routes/route.js"
+import router from "./routes/route.js";
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   router(window.location.hash);
-})
-
-window.addEventListener('hashchange', () => {
-  router(window.location.hash);
-  console.log("Escuchando evento hash");
-
 });
 
+window.addEventListener("hashchange", () => {
+  router(window.location.hash);
+  console.log("Escuchando evento hash");
+});
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
